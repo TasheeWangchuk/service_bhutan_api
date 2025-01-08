ff86e1f0e5c058a10f81de720f5897407a982fc0
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-01-08 17:35:27 +0600

    refactor(service_api):update urls.py

--- service_api/urls.py
+++ service_api/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
+
+
+def redirect_to_api(request):
+    return redirect('api/v1/users/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/users", include("user.urls"), name="users"),
     path("api/v1/portfolios", include("portfolio.urls"), name="portfolio"),
+    path("", redirect_to_api, name="home"),  # Add this line
 ]
