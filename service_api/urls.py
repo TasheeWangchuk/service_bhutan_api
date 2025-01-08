11d094f91156f2746cb4aff22d53f3777998d9e0
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-01-08 17:25:24 +0600

    refactor(service_api):update urls

--- service_api/urls.py
+++ service_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("user.urls"), name="users"),
-    path("api/v1/", include("portfolio.urls"), name="portfolio"),
+    path("api/v1/users", include("user.urls"), name="users"),
+    path("api/v1/portfolios", include("portfolio.urls"), name="portfolio"),
 ]
