c31c9e52a49d94fb432331a142ac457463a29e78
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-02-05 23:46:43 +0600

    refactor(urls): update urls

--- service_api/urls.py
+++ service_api/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("user.urls"), name="users"),
     path("api/v1/portfolios/", include("portfolio.urls"), name="portfolios"),
-    # path("api/v1/", include("job.urls"), name="jobs"),
+    path("api/v1/", include("job.urls"), name="jobs"),
     # path("api/v1/", include("contract.urls"), name="jobs"),
-    
+    path("api/v1/", include("notification.urls"), name="notifications"),
     path("", redirect_to_api, name="home"),  # Add this line
 ]
