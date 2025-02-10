2f82c7cfec97c1f08f0d2b7b2fa5e132c4b3fba8
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-02-10 22:12:37 +0600

    refactor(setting):update setting for redis connection

--- service_api/settings.py
+++ service_api/settings.py
@@ -212,8 +212,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # Default authentication
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
