22fdfc7ce39e48b87d06afa5f039bf55ce23afea
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-02-17 00:24:54 +0600

    refactor(settings):update the settings

--- service_api/settings.py
+++ service_api/settings.py
@@ -217,7 +217,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tashiwangchuk619@gmail.com'
 EMAIL_HOST_PASSWORD = 'nmhd phid hbon uxnq'
-HOST_URL='http://127.0.0.1:8000'
-EFAULT_FROM_EMAIL = 'tashiwangchuk619@gmail.com'
+HOST_URL='https://service-bhutan-api-o2oc.onrender.com'
+DEFAULT_FROM_EMAIL = 'tashiwangchuk619@gmail.com'
 
 
