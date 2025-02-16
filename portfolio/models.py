df9690897013a19b6a85ea091e885be94f84a44d
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-02-16 22:21:30 +0600

    refactor(model):update models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 from django.conf import settings
 from user.models import Profile
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
+class Service(models.Model):
+    service_id = models.AutoField(primary_key=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='services')
+    service_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    class Meta:
+        managed = True
+        db_table = 'services'
+    
 class Portfolio(models.Model):
     portfolio_id = models.AutoField(primary_key=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='portfolios')
     project_title = models.CharField(max_length=255)
-    project_role = models.CharField(blank=True, null=True)
+    project_picture = models.ImageField(upload_to='portfolio/',storage=MediaCloudinaryStorage(),null=True,blank=True)
     project_description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'portfolios'
 
 class Certificate(models.Model):
@@ -20,15 +31,14 @@ class Certificate(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='certificates')
     certificate_title = models.CharField(max_length=255, blank=True, null=True)
     certificate_issuer = models.CharField(max_length=255, blank=True, null=True)
-    certificate_file = models.CharField(blank=True, null=True)
+    certificate_picture = models.ImageField(upload_to='certificate_picture/', storage=MediaCloudinaryStorage, blank=True, null=True)
     issue_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'certificates'
-
 
 class Education(models.Model):
     education_id = models.AutoField(primary_key=True)
@@ -43,9 +53,8 @@ class Education(models.Model):
     
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'educations'
-
 
 class Experience(models.Model):
     experience_id = models.AutoField(primary_key=True)
@@ -61,6 +70,6 @@ class Experience(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'experiences'
 
